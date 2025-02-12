5daf60e7b2f2a4a9b11b1153ab77c02b21022c8a
Kaic Cesar <kaic_cesar@hotmail.com>
2025-02-12 20:36:37 -0300

    Iniciando projeto de desenvolvimento de uma leading page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import '../src/output.css';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg py-10">
+      <h1 className=''>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
